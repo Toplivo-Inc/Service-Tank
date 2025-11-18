066c69eed086db0f136cbcab5c414881d42bb3b5
doom <doomfinland@gmail.com>
2025-11-18 17:00:22 +0300

    Added CLI-args parsing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,32 @@
 use async_trait::async_trait;
 use pingora::prelude::*;
 use std::sync::Arc;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Opt {
+    /// Upstream backends
+    #[arg(short, long, default_values_t = [
+        "1.1.1.1:443".to_string(),
+        "1.0.0.1:443".to_string(),
+    ])]
+    upstreams: Vec<String>,
+
+    /// Addresses for listeners: http & https
+    #[arg(short, long, default_values_t = [
+        "0.0.0.0:6188".to_string(),
+        "0.0.0.0:6189".to_string(),
+    ])]
+    listeners: Vec<String>,
+
+    /// Path to TLS certificate
+    #[arg(short, long, default_value_os_t = format!("{}/tests/keys/server.crt", env!("CARGO_MANIFEST_DIR")))]
+    cert: String,
+
+    /// Path to TLS private key
+    #[arg(short, long, default_value_os_t = format!("{}/tests/keys/key.pem", env!("CARGO_MANIFEST_DIR")))]
+    key: String,
+}
 
 pub struct LB(Arc<LoadBalancer<RoundRobin>>);
 
@@ -11,10 +37,7 @@ impl ProxyHttp for LB {
     fn new_ctx(&self) -> () {}
 
     async fn upstream_peer(&self, _session: &mut Session, _ctx: &mut ()) -> Result<Box<HttpPeer>> {
-        let upstream = self
-            .0
-            .select(b"", 256)
-            .unwrap();
+        let upstream = self.0.select(b"", 256).unwrap();
 
         println!("upstream peer is: {upstream:?}");
 
@@ -28,18 +51,25 @@ impl ProxyHttp for LB {
         upstream_request: &mut RequestHeader,
         _ctx: &mut Self::CTX,
     ) -> Result<()> {
-        upstream_request
-            .insert_header("Host", "one.one.one.one")?;
+        upstream_request.insert_header("Host", "one.one.one.one")?;
         Ok(())
     }
 }
 
 fn main() {
+    let opt = Opt::parse();
+
+    let tcp_http_list = &opt.listeners[0];
+    let tcp_https_list = &opt.listeners[1];
+
+    let cert_path = &opt.cert;
+    let key_path = &opt.key;
+
     let mut my_server = Server::new(None).unwrap();
     my_server.bootstrap();
 
     let mut upstreams =
-        LoadBalancer::try_from_iter(["1.1.1.1:443", "1.0.0.1:443", "127.0.0.1:343"]).unwrap();
+        LoadBalancer::try_from_iter(opt.upstreams).unwrap();
 
     let hc = TcpHealthCheck::new();
     upstreams.set_health_check(hc);
@@ -50,15 +80,15 @@ fn main() {
     let upstreams = background.task();
 
     let mut lb = http_proxy_service(&my_server.configuration, LB(upstreams));
-    lb.add_tcp("0.0.0.0:6188");
+    lb.add_tcp(tcp_http_list);
 
-    let cert_path = format!("{}/tests/keys/server.crt", env!("CARGO_MANIFEST_DIR"));
-    let key_path = format!("{}/tests/keys/key.pem", env!("CARGO_MANIFEST_DIR"));
+    // let cert_path = format!("{}/tests/keys/server.crt", env!("CARGO_MANIFEST_DIR"));
+    // let key_path = format!("{}/tests/keys/key.pem", env!("CARGO_MANIFEST_DIR"));
 
     let mut tls_settings =
         pingora_core::listeners::tls::TlsSettings::intermediate(&cert_path, &key_path).unwrap();
     tls_settings.enable_h2();
-    lb.add_tls_with_settings("0.0.0.0:6189", None, tls_settings);
+    lb.add_tls_with_settings(tcp_https_list, None, tls_settings);
 
     my_server.add_service(lb);
     my_server.add_service(background);
